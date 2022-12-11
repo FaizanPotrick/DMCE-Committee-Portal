8880b94e84fcb93864fc45d49156ff515c7913ff
FaizanPotrick <faizan.potrick02@gmail.com>
2022-12-11 19:09:14 +0530

    Update Package

--- app.js
+++ app.js
@@ -35,7 +35,6 @@ app.use(express.static(path.join(__dirname, "./client/dist")));
 app.use(require("./routes/User.js"));
 app.use(require("./routes/Event.js"));
 app.use(require("./routes/Contact.js"));
-app.use(require("./routes/ChatBot.js"));
 app.use(ErrorHandler);
 
 if (process.env.NODE_ENV === "production") {
